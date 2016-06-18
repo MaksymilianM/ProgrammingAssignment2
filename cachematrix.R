295eeafb45a5db6d3cb8ce2a6f2381ad01643be9
MaksymilianM <MMAC@widex.com>
2016-06-18 19:54:23 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+### makeCacheMatrix
+## makeCacheMatrix function  returns a list of 4 functions:
+## No.1 - set: store inputted matrix in parent enviroment and sets mInv to NULL what is a trigger for 
+##        cacheSolve function and let it check if any computation has been previuosly made for this, exact
+##        matrix.
+## No.2 - get: returns the the matrix.
+## No.3 - setinverseM: saves the result of the computation in the parent environment as the inverseM variable.
+## No.4 - getinverseM: returns inverseM.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mInv <- NULL
+  set <- function(y) {
+    x <<- y
+    mInv <<- NULL
+  }
+  get <- function() x
+  setinverseM <- function(inverseM) mInv <<- inverseM
+  getinverseM <- function() mInv
+  list(set = set, get = get,
+       setinverseM = setinverseM,
+       getinverseM = getinverseM)
 }
 
 
-## Write a short comment describing this function
-
+### cacheSlove
+## cacheSolve function checks mInv value (if is or is not NULL) and return stored inverse of inputted martix
+## (when mInv is not NULL) or makes new computation of it (via solve() function which provide inverse for our 
+##  matrix when mInv is NULL)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mInv <- x$getinverseM()
+  if(!is.null(mInv)) {
+    message("getting cached data")
+    return(mInv)
+  }
+  data <- x$get()
+  mInv <- solve(data, ...)
+  x$setinverseM(mInv)
+  mInv
 }
+
